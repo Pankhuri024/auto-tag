f671c983b674a2131c5c865bc5a157c18c1bd572
Pankhuri024 <pankhurivapta@gmail.com>
2024-12-19 15:20:32 +0530

    19 dec 10

--- app.py
+++ app.py
@@ -124,12 +124,12 @@ def extract_lift_and_metric_ai(summary, goals):
         response = llm(prompt)
         print("response", response)
         
-        # Extract structured data from the response
         try:
             results = json.loads(response.content)
             valid_results = [
                 item for item in results if "lift" in item and "metric" in item and item["metric"] in goals
             ]
+            print("valid_results",valid_results)
             return jsonify(results) 
         except json.JSONDecodeError:
             valid_results = []
